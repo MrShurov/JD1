594ee06ed75f61f987777513e873b68903b7b905
Egor Shurov <shurov13@mail.ru>
2017-07-24 18:18:51 +0300

    12

--- src/Lesson10/Task6.java
+++ src/Lesson10/Task6.java
@@ -2,17 +2,29 @@ package Lesson10;
 
 public class Task6 {
     public static void main(String[] args){
-        String str = "aaabbcdeef";
+        String str = "aaabbcdeef ttt";
         replacement(str);
     }
 
-    public static void replacement(String str){
+    private static void replacement(String str){
+        char[] strChar = str.toCharArray();
+        int counter = 0;
+        int counterStr = 0;
         for(int i = 0; i < str.length() - 1; i++){
-            if(str.charAt(i) == str.charAt(i + 1)){
-                char a = str.charAt(i);
-                str = str.replace(a,' ');
+            if(strChar[i] == strChar[i + 1]){
+                strChar[i] = '^';
+                counterStr++;
             }
         }
+        counterStr = str.length() - counterStr;
+        char[] newStr = new char[counterStr];
+        for(int i = 0; i < str.length(); i++) {
+            if (strChar[i] != '^'){
+                newStr[counter] = strChar[i];
+                counter++;
+            }
+        }
+        str = String.copyValueOf(newStr);
         System.out.println(str);
     }
 
